90abbb28822f133b28edfc40010498886b2b3f3b
OffSey <87211817+OffSey@users.noreply.github.com>
2024-05-20 01:32:20 +0200

    Create fxmanifest.lua
    Add fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+lua54 'yes'
+game 'gta5'
+author 'Luna & OffSey :D'
+description 'zombie-mask convert to ESX'
+version '1.0.0'
+
+
+
+client_scripts {
+	'@ox_lib/init.lua',
+	'shared/config.lua',
+	'client/client.lua'
+}
+
+server_scripts {
+	'shared/config.lua',
+	'server/server.lua',
+}
